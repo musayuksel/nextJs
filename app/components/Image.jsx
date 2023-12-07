bd8f272313a1ee1344025c90d1525cec4c1bc8dd
Musa <79163385+musayuksel@users.noreply.github.com>
2023-12-07 21:49:18 +0000

    Refactor:Move model into a separate component

--- app/components/Image.jsx
+++ app/components/Image.jsx
@@ -1,21 +1,116 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import ImageModal from './ImageModal';
 
 export default function ImageCard({ imgUrl, altDescription }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <li style={{ border: '1px solid red' }}>
-      {/* Image component from NextJS https://nextjs.org/docs/pages/api-reference/components/image */}
-      <Image
-        style={{ objectFit: 'cover' }}
-        src={imgUrl}
-        alt={altDescription}
-        width={200}
-        height={200}
-        loading="lazy"
-        className="rounded"
-      />
-    </li>
+    <>
+      <li style={{ width: '100%', height: '500px' }} onClick={openModal}>
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+          <Image
+            src={imgUrl}
+            alt={altDescription}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            className="rounded"
+            loading="lazy"
+          />
+        </div>
+      </li>
+      {isModalOpen && (
+        <ImageModal
+          imgUrl={imgUrl}
+          altDescription={altDescription}
+          closeModal={closeModal}
+        />
+      )}
+    </>
   );
 }
+
+// import React, { useState } from 'react';
+// import Image from 'next/image';
+
+// export default function ImageCard({ imgUrl, altDescription }) {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const openModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   return (
+//     <>
+//       <li
+//         style={{ border: '1px solid red', width: '100%', height: '500px' }}
+//         onClick={openModal}
+//       >
+//         <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+//           <Image
+//             src={imgUrl}
+//             alt={altDescription}
+//             layout="fill"
+//             objectFit="cover"
+//             objectPosition="center"
+//             className="rounded"
+//             loading="lazy"
+//           />
+//         </div>
+//       </li>
+//       {isModalOpen && (
+//         <div
+//           className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-75 flex justify-center items-center z-50"
+//           onClick={closeModal}
+//         >
+//           <div className="relative">
+//             <Image
+//               src={imgUrl}
+//               alt={altDescription}
+//               layout="intrinsic"
+//               width={800}
+//               height={600}
+//               objectFit="contain"
+//               objectPosition="center"
+//               className="rounded"
+//             />
+//             <button
+//               className="absolute top-4 right-4 bg-white rounded-full p-2"
+//               onClick={closeModal}
+//             >
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 className="h-6 w-6 text-black"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 stroke="currentColor"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               </svg>
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
